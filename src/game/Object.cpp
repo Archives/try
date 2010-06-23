a9190e9d017ea79c6e36aca978a54b6e6473d44d
KillerFrca <killerfrca@himym.cz>
2010-06-23 17:30:14 +0200

    [pr530] Some fixes for non-stop moving transports

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -566,7 +566,7 @@ void Object::BuildValuesUpdate(uint8 updatetype, ByteBuffer * data, UpdateMask *
     }
     else                                                    // case UPDATETYPE_VALUES
     {
-        if (isType(TYPEMASK_GAMEOBJECT) && !((GameObject*)this)->IsTransport())
+        if (isType(TYPEMASK_GAMEOBJECT) && !((GameObject*)this)->IsDynTransport())
         {
             if ( ((GameObject*)this)->ActivateToQuest(target) || target->isGameMaster())
             {
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10091"
- #define REVISION_VP "pr529"
+ #define REVISION_VP "pr530"
  #define REVISION_VP_DATE "23.6.2010"
 #endif // __REVISION_NR_H__
