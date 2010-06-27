a6d772a4d72d474a29966cce45514ee0592e7742
unknown <vbocek@gmail.com>
2010-06-27 13:22:27 +0200

    [pr542] Update location when arrived to final dest in RandomMovementGenerator

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10091"
- #define REVISION_VP "pr541"
+ #define REVISION_VP "pr542"
  #define REVISION_VP_DATE "27.6.2010"
 #endif // __REVISION_NR_H__
