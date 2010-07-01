6545796f85a737af8390eab3875512454e025ea4
KillerFrca <killerfrca@himym.cz>
2010-07-01 16:00:52 +0200

    [pr550] Spells ignoring UNIT_FLAG_STUNNED should ignore only stuns with stun mechanic
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -5811,6 +5811,17 @@ SpellCastResult Spell::CheckCasterAuras() const
         else
             return prevented_reason;
     }
+    
+    // in case of SPELL_ATTR_EX5_USABLE_WHILE_STUNNED must be also checked mechanic of spell
+    if (unitflag & UNIT_FLAG_STUNNED && m_spellInfo->AttributesEx5 & SPELL_ATTR_EX5_USABLE_WHILE_STUNNED)
+    {
+        Unit::AuraMap const& auras = m_caster->GetAuras();
+        for(Unit::AuraMap::const_iterator itr = auras.begin(); itr != auras.end(); ++itr)
+            if (itr->second)
+                if (itr->second->GetModifier()->m_auraname == SPELL_AURA_MOD_STUN)
+                    if (!(GetSpellMechanicMask(itr->second->GetSpellProto(), itr->second->GetEffIndex()) & MECHANIC_STUN))
+                        return SPELL_FAILED_STUNNED;
+    }
     return SPELL_CAST_OK;
 }
 
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10116"
- #define REVISION_VP "pr549"
+ #define REVISION_VP "pr550"
  #define REVISION_VP_DATE "1.7.2010"
 #endif // __REVISION_NR_H__
