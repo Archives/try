960c95815b99426ca25ec69e72c44017b35e6745
KillerFrca <killerfrca@himym.cz>
2010-07-02 20:13:27 +0200

    [pr556] Implement Glyph of Mind Flay and Twisted Faith (part with increased damage of Mind Flay)
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10134"
- #define REVISION_VP "pr555"
+ #define REVISION_VP "pr556"
  #define REVISION_VP_DATE "2.7.2010"
 #endif // __REVISION_NR_H__
