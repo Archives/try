f29bf16f5eeab91df62900b495f053a080a3ecbb
unknown <vbocek@gmail.com>
2010-07-04 15:11:17 +0200

    [pr567] Implement Eye of Kilrogg

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10134"
- #define REVISION_VP "pr566"
- #define REVISION_VP_DATE "3.7.2010"
+ #define REVISION_VP "pr567"
+ #define REVISION_VP_DATE "4.7.2010"
 #endif // __REVISION_NR_H__
