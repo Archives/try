4df4f2da7c4424224a1c694c300043165f801d8f
VladimirMangos <vladimir@getmangos.com>
2010-07-09 22:40:01 +0400

    [10171] Clarify acoount password/lock command errors.

--- src/game/Level0.cpp
+++ src/game/Level0.cpp
@@ -212,7 +212,11 @@ bool ChatHandler::HandleAccountPasswordCommand(const char* args)
 {
     // allow use from RA, but not from console (not have associated account id)
     if (!GetAccountId())
+    {
+        SendSysMessage (LANG_RA_ONLY_COMMAND);
+        SetSentErrorMessage (true);
         return false;
+    }
 
     if(!*args)
         return false;
@@ -267,7 +271,11 @@ bool ChatHandler::HandleAccountLockCommand(const char* args)
 {
     // allow use from RA, but not from console (not have associated account id)
     if (!GetAccountId())
+    {
+        SendSysMessage (LANG_RA_ONLY_COMMAND);
+        SetSentErrorMessage (true);
         return false;
+    }
 
     if (!*args)
     {
