d37e41e41d0814553ebe064c957b5afcefbc85a4
Danstahr <Dan.Stahr@gmail.com>
2010-07-09 22:40:44 +0200

    typo Signed-off-by: Danstahr <Dan.Stahr@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -2,9 +2,5 @@
 #define __REVISION_NR_H__
  #define REVISION_NR "10155"
  #define REVISION_VP "pr584"
- #define REVISION_VP "pr583"
-=======
- #define REVISION_VP "pr584"
->>>>>>> Random Daily Dungeon and Heroic quests are now really random
  #define REVISION_VP_DATE "9.7.2010"
 #endif // __REVISION_NR_H__
