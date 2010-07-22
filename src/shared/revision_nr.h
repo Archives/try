6dff7fe5d933c64c2098512e3184a405e8e3eeaf
KillerFrca <killerfrca@himym.cz>
2010-07-22 20:12:48 +0200

    [pr621] Fix Will of Necropolis (thx MrLama)
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10199"
- #define REVISION_VP "pr620"
+ #define REVISION_VP "pr621"
  #define REVISION_VP_DATE "22.7.2010"
 #endif // __REVISION_NR_H__
