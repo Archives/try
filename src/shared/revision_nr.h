03b453eb2af5fdc5bd739250c7428eb343a379a0
unknown <vbocek@gmail.com>
2010-07-24 20:37:37 +0200

    [pr629] Fix Forethought Talisman and Rejuvenation stacking

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10199"
- #define REVISION_VP "pr628"
+ #define REVISION_VP "pr629"
  #define REVISION_VP_DATE "24.7.2010"
 #endif // __REVISION_NR_H__
