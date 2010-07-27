62de22c4862d4d4fd113cdd2ee111c53629d1dcf
KillerFrca <killerfrca@himym.cz>
2010-07-27 02:04:27 +0200

    [pr643] Pac uz mam tech DK dost zejo
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10199"
- #define REVISION_VP "pr636"
+ #define REVISION_VP "pr643"
  #define REVISION_VP_DATE "26.7.2010"
 #endif // __REVISION_NR_H__
