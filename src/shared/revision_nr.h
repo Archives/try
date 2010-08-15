4d7d82b181d4671129fdf37983fdfc74600ce764
reeshack <richard.galis@gmail.com>
2010-08-15 22:38:26 +0200

    [pr706] Vellums patch (credits to Lynx3d)
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10199"
- #define REVISION_VP "pr705"
+ #define REVISION_VP "pr706"
  #define REVISION_VP_DATE "15.8.2010"
 #endif // __REVISION_NR_H__
