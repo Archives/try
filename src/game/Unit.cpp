4c815c3b0abc1bfdab928c93d93f9a65d6d5669c
unknown <vbocek@gmail.com>
2010-08-15 21:42:11 +0200

    Revert "[pr476] Really stop moving on fear/confused remove"
    This reverts commit 60c6fa59b30c43eabd46fd0e45b68757bdf0387e.
    
    Conflicts:
    
    	src/shared/revision_nr.h

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -14114,7 +14114,7 @@ void Unit::SetFeared(bool apply, uint64 const& casterGUID, uint32 spellID, uint3
     {
         RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_FLEEING);
 
-        GetMotionMaster()->Clear(false, true);
+        GetMotionMaster()->MovementExpired(false);
 
         if( GetTypeId() != TYPEID_PLAYER && isAlive() )
         {
@@ -14154,7 +14154,7 @@ void Unit::SetConfused(bool apply, uint64 const& casterGUID, uint32 spellID)
     {
         RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_CONFUSED);
 
-        GetMotionMaster()->Clear(false, true);
+        GetMotionMaster()->MovementExpired(false);
 
         if (GetTypeId() != TYPEID_PLAYER && isAlive())
         {
