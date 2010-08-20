606edb1e59b1acf9080d923487333fc94b3b142e
unknown <vbocek@gmail.com>
2010-08-20 23:52:28 +0200

    [pr728] Really fix Forethought Talisman and Rejuvenation stacking. Can somebody tell me why this trinket spell has spellfamily druid?

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10272"
- #define REVISION_VP "pr726"
- #define REVISION_VP_DATE "19.8.2010"
+ #define REVISION_VP "pr728"
+ #define REVISION_VP_DATE "20.8.2010"
 #endif // __REVISION_NR_H__
