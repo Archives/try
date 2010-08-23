ca3df913d77e9c088d5c93f679623119713d25ad
reeshack <richard.galis@gmail.com>
2010-08-23 15:31:47 +0200

    [pr735] Mozna vopravi ten reflekt :P mozna ne, ale chyba bude urco nekde tam
    Signed-off-by: reeshack <richard.galis@gmail.com>

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10272"
- #define REVISION_VP "pr734"
+ #define REVISION_VP "pr735
  #define REVISION_VP_DATE "23.8.2010"
 #endif // __REVISION_NR_H__
