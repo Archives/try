e0d46a3d7ae90b5b94ea5cc57fcd78774db02d9f
KillerFrca <killerfrca@himym.cz>
2010-08-23 19:07:34 +0200

    [pr738] Ma to bys script meno ne AI meno mg
    Signed-off-by: KillerFrca <killerfrca@himym.cz>

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -3856,8 +3856,8 @@ bool ChatHandler::HandleNpcInfoCommand(const char* /*args*/)
     PSendSysMessage(LANG_NPCINFO_CHAR,  target->GetDBTableGUIDLow(), faction, npcflags, Entry, displayid, nativeid);
     PSendSysMessage(LANG_NPCINFO_LEVEL, target->getLevel());
     PSendSysMessage(LANG_NPCINFO_HEALTH,target->GetCreateHealth(), target->GetMaxHealth(), target->GetHealth());
-    if (target->HasAIName())
-        PSendSysMessage("ScriptName: %s", target->GetAIName().c_str());
+    if (target->HasScriptName())
+        PSendSysMessage("ScriptName: %s", target->GetScriptName().c_str());
     PSendSysMessage(LANG_NPCINFO_FLAGS, target->GetUInt32Value(UNIT_FIELD_FLAGS), target->GetUInt32Value(UNIT_DYNAMIC_FLAGS), target->getFaction());
     PSendSysMessage(LANG_COMMAND_RAWPAWNTIMES, defRespawnDelayStr.c_str(),curRespawnDelayStr.c_str());
     PSendSysMessage(LANG_NPCINFO_LOOT,  cInfo->lootid,cInfo->pickpocketLootId,cInfo->SkinLootId);
