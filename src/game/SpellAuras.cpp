5936a06f6ca580894cd26d2ff2aaace7c33c0566
unknown <vbocek@gmail.com>
2010-08-23 15:15:48 +0200

    [pr733] Fix hidden stealth ranks

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4685,6 +4685,10 @@ void Aura::HandleModStealth(bool apply, bool Real)
         // only at real aura add
         if (Real)
         {
+            //Player stealths has no logner ranks...could be correct
+            if(GetSpellProto()->EffectRealPointsPerLevel[GetEffIndex()])
+                m_modifier.m_amount = target->getLevel()*GetSpellProto()->EffectRealPointsPerLevel[GetEffIndex()];
+
             target->SetStandFlags(UNIT_STAND_FLAGS_CREEP);
 
             if (target->GetTypeId()==TYPEID_PLAYER)
