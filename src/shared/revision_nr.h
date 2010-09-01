0b1037873f641155658919b5c7c4d8b0e63c1ce2
KillerFrca <killerfrca@himym.cz>
2010-09-01 17:20:52 +0200

    pyco

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
  #define REVISION_NR "10272"
- #define REVISION_VP "pr750"
+ #define REVISION_VP "pr751"
  #define REVISION_VP_DATE "1.9.2010"
 #endif // __REVISION_NR_H__
